7a0e554c9c49b6c03d0004fda52919a7741fcc5e
Cedric Verstraeten <cedric@verstraeten.io>
2022-10-04 22:03:06 +0200

    fix

--- src/components/Timesup.js
+++ src/components/Timesup.js
@@ -13,9 +13,10 @@ export default function Timesup({ userName, setTimeOut, setTimer, handleNextQues
     };
 
     return (
-        <div>
+        <div className="run-out-of-time">
             <div className="timesup">
                 <h1>You have no time left &#128336;!</h1><br/>
+                <h3>No points for you this time</h3>
                 <button 
                     onClick={handleClick}
                 >Next question</button>
