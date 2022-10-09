a3a9cf6feaabbfeaef5af9ef343c8bf4e7cb8901
Cedric Verstraeten <cedric@verstraeten.io>
2022-10-09 19:31:13 +0200

    Update QuestionsDatamindsConnect.js

--- src/components/QuestionsDatamindsConnect.js
+++ src/components/QuestionsDatamindsConnect.js
@@ -1,157 +1,499 @@
 export default  [
 
-  /* Julien session */
   {
-    "category": "Julien",
+    "category": "PowerBI",
     "type": "multiple",
     "difficulty": "hard",
-    "question": "Which one of these Microsoft subsidiaries doesn't noticeably use Java?",
-    "correct_answer": "id Software",
+    "question": "When was the first version of PowerBI released?",
+    "correct_answer": "2013",
       "incorrect_answers": [
-        "Minecraft",
-        "Yammer",
-        "LinkedIn"
+        "2012",
+        "2014",
+        "2015"
       ]
   },
   {
-    "category": "Julien",
+    "category": "PowerBI",
     "type": "multiple",
     "difficulty": "hard",
-    "question": "In which language is coded JHipster?",
-    "correct_answer": "JavaScript",
+    "question": "What is Power Pivot?",
+    "correct_answer": "It extends the analytical capabilities and services of Microsoft Excel.",
       "incorrect_answers": [
-        "Java",
-        "Go",
-        "Bash"
+        "It helps to create complex pivot tables",
+        "It's the previous name of Power BI",
+        "It doesnt exists at all."
       ]
   },
   {
-    "category": "Julien",
+    "category": "PowerBI",
     "type": "multiple",
     "difficulty": "hard",
-    "question": "In JavaScript, what is the result of \"0.1+0.2\"?",
-    "correct_answer": "0.30000000000000004",
+    "question": "What is GetData in Power BI?",
+    "correct_answer": "Get Data is a simple icon on Power BI used to import data from the source.",
       "incorrect_answers": [
-        "0.3",
-        "0",
-        "NaN"
-      ]
-  },
-
-  /* Chris session */
-  {
-    "category": "Chris",
-    "type": "multiple",
-    "difficulty": "hard",
-    "question": "Which answer does not correctly describe the new, larger GitHub-hosted runners?",
-    "correct_answer": "Java installed by default",
-      "incorrect_answers": [
-        "Available for Linux and Windows",
-        "Static IP ranges available",
-        "Auto-scaled runners by GitHub"
+        "It's a custom function to call an external API",
+        "This concept doesnt exists in PowerBI",
+        "GetData is a short-cut to combine 2 excels"
       ]
   },
   {
-    "category": "Chris",
+    "category": "PowerBI",
     "type": "multiple",
     "difficulty": "hard",
-    "question": "How would you define a set of recommended extensions for a Codespace?",
-    "correct_answer": "Configure them in the extensions property of devcontainer.json",
+    "question": "Business Analysis, Data Analysis, Data Science",
+    "correct_answer": "2013",
       "incorrect_answers": [
-        "Install them in the dockerfile",
-        "Setup a script to install in the dotfiles",
-        "Use settings sync to install the specific extensions"
+        "Business Analysis, Data Analysis, Application Development",
+        "Mobile Applications, Data Analysis, Integration",
+        "Data Analytics"
       ]
   },
-  /* !!!!!!!!!!!!!!!!!!!*/
   {
-    "category": "Chris",
+    "category": "PowerBI",
     "type": "multiple",
     "difficulty": "hard",
-    "question": "Which answer does not correctly describe code scanning in GitHub Advanced Security?",
-    "correct_answer": "Configure them in the extensions property of devcontainer.json",
-    "incorrect_answers": [
-      "Third-party analysis tools can be run, and results imported to GitHub Code Scanning",
-      "Third-party tools that output into a CSV format can be integrated with Code Scanning",
-      "Use settings sync to install the specific extensions"
-    ]
+    "question": "What is 'M language'?",
+    "correct_answer": "A programming language used in Power Query",
+      "incorrect_answers": [
+        "A language to write machine learning algorithms",
+        "M programming language doesnt exists",
+        "M language is a dialect used in the middle-east."
+      ]
   },
   {
-    "category": "Chris",
+    "category": "PowerBI",
     "type": "multiple",
     "difficulty": "hard",
-    "question": "What is Github Artifacts?",
-    "correct_answer": "A way to store data produced by a workflow",
+    "question": "What does DAX stand for?",
+    "correct_answer": "Data Analysis Expression",
       "incorrect_answers": [
-        "A cloud storage solution from Github",
-        "A place to store source code",
+        "Data Annotation Example",
+        "Data And Excel",
+        "Data Analysis Experience"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is the name of the functionality that allows users to ask natural language questions and get answers in the form of a visual?",
+    "correct_answer": "Q&A",
+      "incorrect_answers": [
+        "Ask Power BI",
+        "Power Answer",
+        "Visualize Me"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is NOT the name of a Power BI license?",
+    "correct_answer": "Power BI Basic",
+      "incorrect_answers": [
+        "Power BI Free",
+        "Power BI Pro",
+        "Power BI Premium per User"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is NOT a dataset mode in the Power BI Service?",
+    "correct_answer": "Real-time",
+      "incorrect_answers": [
+        "Import",
+        "Composite",
+        "DirectQuery"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What does 'M' in 'M Language' in Power Query stands for?",
+    "correct_answer": "Data Mashup Language",
+      "incorrect_answers": [
+        "Data Manipulation Language",
+        "Data Minor Language",
+        "Mostly used to cleanse data Language"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which one of the following is considered to be an AI visual?",
+    "correct_answer": "Key Influencer Visual",
+      "incorrect_answers": [
+        "Combo Chart",
+        "Azure Map",
+        "Deneb"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Whay is a Premium only capability?",
+    "correct_answer": "Bring Your Own Key",
+      "incorrect_answers": [
+        "PBI Desktop Incremental Refresh",
+        "8 Scheduled refreshes a day",
+        "Models lower than 1 GB"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What are the Analytics Runtimes available in Azure Synapse?",
+    "correct_answer": "Apache Spark, Data Explorer, SQL",
+      "incorrect_answers": [
+        "SQL, Apache Spark",
+        "Apache Spark, Data Explorer",
+        "SQL, Data Explorer"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is NOT a type of filter in a Power BI Report?",
+    "correct_answer": "App-level Filters",
+      "incorrect_answers": [
+        "Page-level Filters",
+        "Report-level Filters",
+        "Visual-level Filters"
+      ]
+  },
+
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "When was the first version of PowerBI released?",
+    "correct_answer": "2013",
+      "incorrect_answers": [
+        "2012",
+        "2014",
+        "2015"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What does .pbit stand for?",
+    "correct_answer": "Power BI Template",
+      "incorrect_answers": [
+        "Power BI Information Technology",
+        "Power BI Technology",
+        "Power BI Tool"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What CAN'T you do with Power Query?",
+    "correct_answer": "If then else conditions",
+      "incorrect_answers": [
+        "Join and append data from multiple data sources",
+        "Import Data from wide range of sources",
+        "Shape data"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which type of chart should be avoided when communicating data?",
+    "correct_answer": "Pie charts",
+      "incorrect_answers": [
+        "Bar charts",
+        "Line charts",
+        "Area charts"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which Azure SQL Deployment option(s) belong to the PaaS (Platform-as-a-Service) family?",
+    "correct_answer": "Azure SQL Managed Instance + Azure SQL Database",
+      "incorrect_answers": [
+        "SQL Server on Azure VMs + Azure SQL Database",
+        "SQL Server on Azure VMs + Azure SQL Managed Instance",
+        "All the deployment options"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which technology is often referred to as the power of blockchain with the simplicity of SQL?",
+    "correct_answer": "Ledger",
+      "incorrect_answers": [
+        "Data Studio",
+        "TDE",
+        "TLS"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What best describes the Azure Data Explorer service?",
+    "correct_answer": "Big data analytics cloud platform optimized for high volume, interactive queries.",
+      "incorrect_answers": [
+        "Big data storage data optimized for batching non-interactive queries.",
+        "Big data solution for data transformations.",
+        "Data science platform"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which of the following is an appropriate use case for Azure Data Explorer?",
+    "correct_answer": "Log and Text analytics",
+      "incorrect_answers": [
+        "In-memory batching processing of petabytes of information.",
+        "Real-time analytics",
+        "Deploying machine learning models"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which aspect of an extract, transform, and load (ETL) process makes it unsuitable for Azure Data Explorer.",
+    "correct_answer": "High query concurrency.",
+      "incorrect_answers": [
+        "Long running tasks",
+        "Large volumes of data for ingestion.",
+        "Microbatching"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What does Microsoft Purview do with the data it discovers from your registered sources?",
+    "correct_answer": "It catalogs and classifies the data that is scanned.",
+      "incorrect_answers": [
+        "It moves the data to your Azure subscription, automatically creating the necessary storage accounts.",
+        "It performs data transformations to match your on-premises schemas.",
+        "It makes your data publicly available"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What aspect of Microsoft Purview is used to configure the data discovery for your data sources?",
+    "correct_answer": "Scan rules",
+      "incorrect_answers": [
+        "Collections",
+        "Classifications",
+        "Resource sets"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which Data Factory component should you use to represent data that you want to ingest for processing?",
+    "correct_answer": "Datasets",
+      "incorrect_answers": [
+        "Linked services",
+        "Pipelines"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which Azure data service would you use to create a graph database?",
+    "correct_answer": "Azure Cosmos DB",
+      "incorrect_answers": [
+        "Azure Blob",
+        "Azure Table",
+        "Azure Factory"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is the range of nodes that a dedicated pool in Synapse have?",
+    "correct_answer": "1 to 60",
+      "incorrect_answers": [
+        "1 to 100",
+        "1 to 40",
+        "1 to 80"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which data redundancy in Azure would you use if you want your data to be repliacated across regions?",
+    "correct_answer": "GRS (Geo-redundant)",
+      "incorrect_answers": [
+        "LRS (Locally redundant)",
+        "ZRS (Zone redunandant)",
+        "CRS (Country redundant)"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which distribution type is recommended when working on a dimension table in a dedicated pool?",
+    "correct_answer": "Replicated",
+      "incorrect_answers": [
+        "Hash",
+        "Robin",
+        "Tresor"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which SAP connector in Azure leverages the SAP CDC (Change data capture) capability?",
+    "correct_answer": "SAP ODP connector",
+      "incorrect_answers": [
+        "SAP HANA connector",
+        "SAP BW connector",
+        "SAP BTP connector"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is the unique value Prop Microsoft can bring in a SQL migration scenario?",
+    "correct_answer": "Leveraging License investments from on-prem into Azure SQL PaaS services",
+      "incorrect_answers": [
+        "License mobility",
+        "Free Assessment tooling for Discovery & Planning",
+        "Assessment tooling for application profiling"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which feature is not present in Azure Cognitive Services for Language?",
+    "correct_answer": "Content Generation",
+      "incorrect_answers": [
+        "(Custom) Named Entity Recognition",
+        "(Custom) Text Classification",
+        "PII Detection"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which role aside from Insights Reader allows you to view Data Estate Insights when given access on a root level?",
+    "correct_answer": "Data Curator",
+      "incorrect_answers": [
+        "Data Reader",
+        "Data Source Admin",
+        "Account Creator"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which of the following is not a type of Integration Runtime in Azure Data Factory?",
+    "correct_answer": "Azure-Synapse Integration Runtime",
+      "incorrect_answers": [
+        "Azure Integration Runtime",
+        "Self-Hosted Integration Runtime",
+        "Azure-SSIS Integration Runtime"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which languages are supported in Azure Synapse Analytics Spark Notebooks?",
+    "correct_answer": "Python, Scala, SQL, C#",
+      "incorrect_answers": [
+        "Python, R, Scala, SQL",
+        "Python, R, SQL, C#",
+        "Python, R, Scala, C#"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "The Delta Lake framework provides ACID guarantees. What does ACID stand for?",
+    "correct_answer": "atomicity, consistency, isolation, and durability",
+      "incorrect_answers": [
+        "atomicity, credibility, isolation, and durability",
+        "atomicity, consistency, integration, and durability",
+        "atomicity, credibility, integration, and durability"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which data processing technique is optimized to manage large volumes of data?",
+    "correct_answer": "Extract, Load, Transform (ELT)",
+      "incorrect_answers": [
+        "Extract, Transform, Load (ETL)",
+        "Stream processing",
+        "ETL & Stream processing"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which one of the following visuals displays major contributors to a selected result or value?",
+    "correct_answer": "Key influencers",
+      "incorrect_answers": [
+        "Matrix Chart",
+        "Column and bar chart",
+        "Scatter plot"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which Azure data services support Apache Spark clusters? ",
+    "correct_answer": "Azure Databricks",
+      "incorrect_answers": [
+        "Azure Cosmos DB",
+        "Azure Data Factory",
+        "All of them"
+      ]
+  },
+  {
+    "category": "PowerBI",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Which component of an Azure Data Factory can be triggered to run data ingestion tasks?",
+    "correct_answer": "Pipelines",
+      "incorrect_answers": [
+        "Datasets",
+        "Linked service",
+        "None of the above"
       ]
   },
 
 
 
 
-  
-
-
-    {
-      "category": "Github Actions",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "Where are Github actions?",
-      "correct_answer": "A CI/CD pipeline for Github",
-        "incorrect_answers": [
-          "An action a developer has to take",
-          "A change request for a Github repository",
-          "Githubs annual event"
-        ]
-    },
-    {
-      "category": "Azure",
-      "type": "multiple",
-      "difficulty": "hard",
-      "question": "Where can I deploy my Maven-packaged Spring Boot application?",
-      "correct_answer": "All off above",
-        "incorrect_answers": [
-          "App Service",
-          "VM",
-          "Spring Cloud"
-        ]
-      },
-      {
-        "category": "Azure",
-        "type": "multiple",
-        "difficulty": "hard",
-        "question": "What is the license of Java binaries?",
-        "correct_answer": "GPLv2",
-          "incorrect_answers": [
-            "Apache 2.0",
-            "MIT",
-            "Commercial License"
-          ]
-        },
-        {
-          "category": "Azure",
-          "type": "multiple",
-          "difficulty": "hard",
-          "question": "Which one(s) of these Azure solutions can you use to deploy containers?",
-          "correct_answer": "All off above",
-            "incorrect_answers": [
-              "Azure Container Instances",
-              "Azure Kubernetes Service",
-              "Azure Container Apps"
-            ]
-          },
-        {
-          "category": "Azure",
-          "type": "multiple",
-          "difficulty": "hard",
-          "question": "Which Azure Messaging solution supports JMS?",
-          "correct_answer": "Azure Service Bus",
-            "incorrect_answers": [
-              "Azure Event Grid",
-              "Azure Event Hub",
-              "Azure Queue Storage"
-            ]
-          }
-  ]
+]
