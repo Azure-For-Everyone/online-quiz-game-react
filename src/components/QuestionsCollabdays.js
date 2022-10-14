8fe7639ac5033dbacd816546e33f8af91fabad27
Cedric Verstraeten <cedric@verstraeten.io>
2022-10-14 16:45:15 +0200

    Update QuestionsCollabdays.js

--- src/components/QuestionsCollabdays.js
+++ src/components/QuestionsCollabdays.js
@@ -1,7 +1,90 @@
 export default  [
 
   {
-    "category": "PowerBI",
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What are the 4 components that are included in the Power Platform?",
+    "correct_answer": "Power BI, Power Apps, Power Automate and Power Virtual Agents",
+      "incorrect_answers": [
+        "Power BI, Power Apps",
+        "Power BI, Power Apps, Power Automatic",
+        "Power BI, Power Apps, Power Automatic and Power Agents"
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is Power Platform used for?",
+    "correct_answer": "low-code application development, workflow automation, AI bot development, and data analytics",
+      "incorrect_answers": [
+        "low-code application development",
+        "low-code application development, workflow automation",
+        "low-code application development, AI bot development"
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Is Power Platform SaaS or PaaS?",
+    "correct_answer": "PaaS",
+      "incorrect_answers": [
+        "SaaS",
+        "No, it's IaaS",
+        "It's just a library"
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What language is Power Platform?",
+    "correct_answer": "Power Fx",
+      "incorrect_answers": [
+        "Power Function",
+        "Power Language",
+        "Power Code"
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Is Power Fx open source?",
+    "correct_answer": "Yes",
+      "incorrect_answers": [
+        "No, it's licensed.",
+        "It depends if you are student.",
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Can you explain what the Common Data Service is?",
+    "correct_answer": "The Common Data Service is a cloud-based data service that allows applications to connect to common data sources.",
+      "incorrect_answers": [
+        "The Common Data Service is a library to create a Restful API.",
+        "The Common Data Service does not exists.",
+        "The Common Data Service is still on the roadmap."
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What are entities in PowerApps?",
+    "correct_answer": "Entities in PowerApps are used to store and manage data.",
+      "incorrect_answers": [
+        "Entities are interfaces within your PowerApp.",
+        "Entities are visual widgets which you can drag into your viewport.",
+        "Entities are used for caching mainly."
+      ]
+  },
+  {
+    "category": "PowerPlatform",
     "type": "multiple",
     "difficulty": "hard",
     "question": "When was the first version of PowerBI released?",
@@ -11,5 +94,259 @@ export default  [
         "2014",
         "2015"
       ]
-    }
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is the threshold limit of subsites in a site in SharePoint 2019?",
+    "correct_answer": "2000",
+      "incorrect_answers": [
+        "2500",
+        "1500",
+        "3000"
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Office 365 is IaaS or SaaS or PaaS?",
+    "correct_answer": "SaaS",
+      "incorrect_answers": [
+        "PaaS",
+        "IaaS",
+        "It's just a program."
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What do you mean by MOSS?",
+    "correct_answer": "MOSS refers to Microsoft Office SharePoint Server",
+      "incorrect_answers": [
+        "MOSS refers to Microsoft Office Security Server",
+        "MOSS refers to Microsoft Office SharePoint System",
+        "MOSS refers to Microsoft Office SharePoint Service"
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is the difference between SharePoint and OneDrive?",
+    "correct_answer": "SharePoint stores business documents for a team in a Team site, OneDrive stores business documents for your own use.",
+      "incorrect_answers": [
+        "OneDrive stores business documents for a team in a Team site, SharePoint stores business documents for your own use.",
+        "OneDrive is for non-business usage.",
+        "SharePoint and OneDrive are the same thing."
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Can an event receiver be deployed through a SharePoint feature?",
+    "correct_answer": "Yes",
+      "incorrect_answers": [
+        "No",
+        "Event receivers do not exist",
+        "Yes, if deployed on a tuesday"
+      ]
+  },
+  {
+    "category": "PowerPlatform",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Why is the Patch function used in canvas Power Apps?",
+    "correct_answer": "Patch function is used to add or modify records in a data source.",
+      "incorrect_answers": [
+        "Patch function is used to modify records in a data source.",
+        "Patch function is used to modify the visual view.",
+        "Patch function is not known in Power Apps"
+      ]
+  },
+  {
+    "category": ".NET",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What does LINQ stands for?",
+    "correct_answer": "Language Integrated Query",
+      "incorrect_answers": [
+        "Language Integration Query",
+        "Lexical Integrated Query",
+        "Language Integrated Qode"
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is SSS in SharePoint?",
+    "correct_answer": "Secure Store Service",
+      "incorrect_answers": [
+        "Security Store Service",
+        "Safety Store Service",
+        "Secure Store Stream"
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is impersonation in SharePoint?",
+    "correct_answer": "Something is executed in the context of a different identity.",
+      "incorrect_answers": [
+        "Something is executed in the context of a different identity.",
+        "A task to authenticate yourself against AAD.",
+        "Impersonation is not possible in SharePoint."
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How to make the site public in SharePoint?",
+    "correct_answer": "To make it public, a user has to enable anonymous access for the entire site.",
+      "incorrect_answers": [
+        "Sites are public by default, nothing to do here.",
+        "It's not possible to make it public.",
+        "Sites can only be made public for specific people."
+      ]
+  },
+  {
+    "category": "SharePoint",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is CAML in SharePoint?",
+    "correct_answer": "Collaborative Application Markup Language",
+      "incorrect_answers": [
+        "Collaborative Application Management Language",
+        "Code Application Markup Language",
+        "CollaborAtive Markup Language"
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is Microsoft Viva Learning?",
+    "correct_answer": "Viva Learning is a centralized learning hub in Microsoft Teams that lets you seamlessly integrate learning and building skills into your day",
+      "incorrect_answers": [
+        "Viva is the next version and rebrading of Microsoft Teams.",
+        "Microsoft Viva Learning does not exists.",
+        "Microsoft Viva Learning is the new name for LinkedIn."
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many languages does Microsoft Viva Learning support?",
+    "correct_answer": "27",
+      "incorrect_answers": [
+        "7",
+        "17",
+        "37"
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is a prerequisite for Microsoft Viva Learning?",
+    "correct_answer": "Microsoft Teams deployed in your organization",
+      "incorrect_answers": [
+        "You don't need anything, Microsoft Viva Learning is a stand-a-lone product.",
+        "You need a Microsoft 365 learning license.",
+        "You need a SharePoint Server 2020 installation."
+      ]
+  },
+
+
+  {
+    "category": "Teams",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is a Teams channel?",
+    "correct_answer": "A virtual place to allow teammates to have focused conversations about specific topics.",
+      "incorrect_answers": [
+        "A public channel that is accessible by every employee.",
+        "The concept of a channel does not exists, it's a room.",
+        "Teams channels are created by default, and you cannot customize them."
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "Is Microsoft Teams only accessible from the browser?",
+    "correct_answer": "The Microsoft Teams app can be used on Windows, Mac, Linux (PWA), Android, & iOS.",
+      "incorrect_answers": [
+        "The Microsoft Teams app can be downloaded for Windows only",
+        "The Microsoft Teams app can be downloaded for Window and Mac.",
+        "The Microsoft Teams app can be downloaded for Window, Android & iOS."
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many languages does Microsoft Viva Learning support?",
+    "correct_answer": "27",
+      "incorrect_answers": [
+        "7",
+        "17",
+        "37"
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many languages does Microsoft Viva Learning support?",
+    "correct_answer": "27",
+      "incorrect_answers": [
+        "7",
+        "17",
+        "37"
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many languages does Microsoft Viva Learning support?",
+    "correct_answer": "27",
+      "incorrect_answers": [
+        "7",
+        "17",
+        "37"
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many languages does Microsoft Viva Learning support?",
+    "correct_answer": "27",
+      "incorrect_answers": [
+        "7",
+        "17",
+        "37"
+      ]
+  },
+  {
+    "category": "Viva",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many languages does Microsoft Viva Learning support?",
+    "correct_answer": "27",
+      "incorrect_answers": [
+        "7",
+        "17",
+        "37"
+      ]
+  },
 ]
