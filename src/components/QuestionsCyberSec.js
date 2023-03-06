225747c09dc27665c133dab4c99b17f96a60cd76
Cedric Verstraeten <cedric@verstraeten.io>
2023-03-06 15:20:42 +0100

    add more questions

--- src/components/QuestionsCyberSec.js
+++ src/components/QuestionsCyberSec.js
@@ -4,9 +4,9 @@ export default  [
     "type": "multiple",
     "difficulty": "hard",
     "question": "What are the three goals of cybersecurity?",
-    "correct_answer": "Confidentiality, identity, authorization.",
+    "correct_answer": "Confidentiality, integrity, and availability.",
       "incorrect_answers": [
-        "Confidentiality, integrity, and availability.",
+        "Confidentiality, identity, authorization.",
         "Confidentiality, identity, authorization.",
         "Authentication, identity, authorization."
       ]
@@ -16,9 +16,9 @@ export default  [
     "type": "multiple",
     "difficulty": "hard",
     "question": "Which type of attack employs malicious websites or browser extensions to get users to download malicious software on their devices?",
-    "correct_answer": "Social Engineering",
+    "correct_answer": "Browser",
       "incorrect_answers": [
-        "Browser",
+        "Social Engineering",
         "Password",
         "SQL Injection."
       ]
@@ -33,6 +33,114 @@ export default  [
         "Browser security",
         "Threat intelligence",
         "Passwordless"
+      ]
+  },
+  {
+    "category": "describe basic cryptography",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "How many keys are required when using symmetric encryption?",
+    "correct_answer": "One",
+      "incorrect_answers": [
+        "Two",
+        "Three",
+        "Four"
+      ]
+  },
+  {
+    "category": "describe basic cryptography",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "When using asymmetric encryption, which key shouldn't be shared?",
+    "correct_answer": "Private key",
+      "incorrect_answers": [
+        "Public Key",
+        "Shared Key",
+        "Key pair"
+      ]
+  },
+  {
+    "category": "describe basic cryptography",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is a hashing function?",
+    "correct_answer": "A hashing function is an algorithm that creates a fixed-length hexadecimal value of the plaintext.",
+      "incorrect_answers": [
+        "A hashing function is an algorithm that creates a new ciphertext message from the plaintext.",
+        "A hashing function is an algorithm that's used to decrypt ciphertext from the sender.",
+        "A hashing function is an algorithm that creates a new key pair."
+      ]
+  },
+  {
+    "category": "describe authentication",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What are the three types of authentication?",
+    "correct_answer": "Something you know, something you have, and something you are.",
+      "incorrect_answers": [
+        "Something you own, something you know, something you learn.",
+        "Something you are, something you have, something you know.",
+        "Something you find, something you are, something you know."
+      ]
+  },
+  {
+    "category": "describe authentication",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is credential stuffing?",
+    "correct_answer": "There's a security breach at one site, and attackers use your compromised usernames and passwords at many other sites.",
+      "incorrect_answers": [
+        "An attacker attempts to gain access using millions of username and password combinations.",
+        "A collective hacking group attempts to gain access to a large number of accounts.",
+        "A dictionary of commonly used words is used for the brute force attack."
+      ]
+  },
+  {
+    "category": "describe authentication",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What are the three principles of Zero Trust?",
+    "correct_answer": "Verify explicitly, assume breach, and use least privileged access.",
+      "incorrect_answers": [
+        "Spear-fishing, pretexting, and baiting.",
+        "Credential stuffing, key logging, spear-fishing.",
+        "Trust the network, trust the user, trust the device."
+      ]
+  },
+  {
+    "category": "describe network-based-threats",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "In a network, what is the fundamental building block that allows multiple devices to communicate with each other?",
+    "correct_answer": "The switch",
+      "incorrect_answers": [
+        "The router",
+        "The access point",
+        "The hub"
+      ]
+  },
+  {
+    "category": "describe network-based-threats",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "What is the name of a common network attack where the cybercriminal compromises a router in the network to eavesdrop on, or alter, data?",
+    "correct_answer": "Man-in-the-middle",
+      "incorrect_answers": [
+        "Bluejacking",
+        "Wardriving",
+        "TCPDrilling"
+      ]
+  },
+  {
+    "category": "describe network-based-threats",
+    "type": "multiple",
+    "difficulty": "hard",
+    "question": "When considering security, what is the common name given to separating your network into discrete entities?",
+    "correct_answer": "Network segmentation",
+      "incorrect_answers": [
+        "Network access control=",
+        "PPNP",
+        "Virtual private network"
       ]
   },
 ]
